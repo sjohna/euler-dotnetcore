2126abc95a65513c4da5258662cb9f8786f5dd05
John A. Stevens <john@otherskylines.com>
2020-05-29 17:19:40 -0600

    Add project Euler #1

---
+++ Euler1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Enumerable.Range(1,999).Where(n => n % 3 == 0 || n % 5 == 0).Sum());
+        }
+    }
+}
