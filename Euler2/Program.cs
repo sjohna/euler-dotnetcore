93f2490c4e041d67e4a20841640b152da7bd3aa5
John A. Stevens <john@otherskylines.com>
2020-05-29 17:41:20 -0600

    Add project Euler #2
    Add solution file, and update build task to build solution.
    
    Update debug configuration in launch.json to debug dll for currently
    open file.

---
+++ Euler2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Euler2
+{
+    class Program
+    {
+        static IEnumerable<int> Fibonacci() {
+            int curr = 1;
+            int prev = 0;
+
+            while(true) {
+                yield return curr;
+                int temp = prev;
+                prev = curr;
+                curr += temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci().TakeWhile(n => n < 4000000).Where(n => n % 2 == 0).Sum());
+        }
+    }
+}
