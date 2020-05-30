ce22362ec4188092d868d5c138d958240cd2be25
John A. Stevens <john@otherskylines.com>
2020-05-30 09:43:21 -0600

    Add project Euler #6

---
+++ Euler6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int,int> square = x => x*x;
+            Console.WriteLine(square(Enumerable.Range(1,100).Sum()) - Enumerable.Range(1,100).Select(x => square(x)).Sum());
+        }
+    }
+}
