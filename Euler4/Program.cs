919a913439cb4d9615a6b6b829bb2f5f46ffda8a
John A. Stevens <john@otherskylines.com>
2020-05-30 07:55:09 -0600

    Add project Euler #4

---
+++ Euler4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Euler4
+{
+    static class Program
+    {
+        static void ConsoleWriteLine(this object o) 
+        {
+            Console.WriteLine(o);
+        }
+
+        // This will fail for some non-ASCII strings...
+        static string Reverse(this string s) 
+        {
+            var charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        static bool IsPalindromeNumber(this int number) 
+        {
+            return number.ToString().Reverse() == number.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            (
+            from x in Enumerable.Range(100,899)
+            from y in Enumerable.Range(100,899)
+            where (x*y).IsPalindromeNumber()
+            select x * y
+            )
+            .Max()
+            .ConsoleWriteLine();
+        }
+    }
+}
