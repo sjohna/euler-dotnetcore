825eaae21b42279e41b2a73a2028eec13b0d71d3
John A. Stevens <john@otherskylines.com>
2020-05-30 17:27:17 -0600

    Add project Euler #10

---
+++ Euler10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+using static Euler.Sequence;
+using static Euler.Extension;
+
+namespace Euler10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Primes().TakeWhile(p => p < 2000000).Sum().ConsoleWriteLine();
+        }
+    }
+}
