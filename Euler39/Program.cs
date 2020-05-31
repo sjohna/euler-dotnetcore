f66c5926769c4138e72c7ad2d3c3ce174f5db064
John A. Stevens <john@otherskylines.com>
2020-05-30 18:16:38 -0600

    Add project Euler #39

---
+++ Euler39/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+using static Euler.Sequence;
+using static Euler.Extension;
+
+namespace Euler39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrossSelect(                                                                            
+                ClosedRange(1,1000),
+                ClosedRange(1,1000),
+                (l,r) => (a: l, b: r, c: Math.Sqrt(l.Squared() + r.Squared()))
+            )
+            .Where(pt => pt.a + pt.b + pt.c < 1000)
+            .Where(pt => pt.c % 1 == 0)
+            .Select(pt => pt.a + pt.b + (long)(pt.c))
+            .GroupBy(p => p)
+            .OrderByDescending(g => g.Count())
+            .First()
+            .Key
+            .ConsoleWriteLine();
+        }
+    }
+}
