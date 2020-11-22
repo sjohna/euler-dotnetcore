e285d83d94e8abab063eb1113d188e762850f9c2
John A. Stevens <john@otherskylines.com>
2020-11-22 11:14:18 -0700

    Update problem 1 parameterization so it's consistent with problem definition

--- Euler001/Program.cs
+++ Euler001/Program.cs
@@ -15,15 +15,15 @@ namespace Euler1
             FunctionChain(999).ConsoleWriteLine();
         }
 
-        public static long FunctionChain(long max)
+        public static long FunctionChain(long limit)
         {
-            return ClosedRange(1, max).Where(n => n % 3 == 0 || n % 5 == 0).Sum();
+            return ClosedRange(1, limit - 1).Where(n => n % 3 == 0 || n % 5 == 0).Sum();
         }
 
-        public static long ForLoop(long max)
+        public static long ForLoop(long limit)
         {
             long sum = 0;
-            for (long num = 1; num <= max; ++num)
+            for (long num = 1; num < limit; ++num)
             {
                 if (num % 3 == 0 || num % 5 == 0)
                 {
@@ -40,11 +40,11 @@ namespace Euler1
             {
                 var factory = EulerProblemInstance<long>.InstanceFactory<long>(typeof(Euler1.Program), 1);
 
-                yield return factory(nameof(FunctionChain), 999L, 233168L).Canonical();
-                yield return factory(nameof(ForLoop), 999L, 233168L);
+                yield return factory(nameof(FunctionChain), 1000L, 233168L).Canonical();
+                yield return factory(nameof(ForLoop), 1000L, 233168L);
 
-                yield return factory(nameof(FunctionChain), 9L, 23L).Mini();
-                yield return factory(nameof(ForLoop), 9L, 23L).Mini();
+                yield return factory(nameof(FunctionChain), 10L, 23L).Mini();
+                yield return factory(nameof(ForLoop), 10L, 23L).Mini();
             }
         }
     }
